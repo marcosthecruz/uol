31aefb2ccd6e21de6dd168f584c1fbc8982e9f36
marcosthecruz <marcosthecruz@gmail.com>
2020-09-08 23:42:16 -0300

    desafio UOL 2a versão

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,7 @@ require "capybara/cucumber"
 require "selenium-webdriver"
 
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = :selenium#_chrome
   config.app_host = "https://www.livelo.com.br"
   config.default_max_wait_time = 15
 end
